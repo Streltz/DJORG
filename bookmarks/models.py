87ff508ba8455f3393ace0312ac61ef90990e94e
Mike Streltsoff <MStreltsoff@gmail.com>
2018-05-22 18:59:28 -0400

    Started coding bookmarks: added models to models.py and bookmarks to the settings.py files

--- bookmarks/models.py
+++ bookmarks/models.py
@@ -1,3 +1,7 @@
 from django.db import models
 
-# Create your models here.
+class Bookmark(models.Model):
+    id = models.UUIDField(primary_key = TRUE, default = uuid4, editable = FALSE)
+    url = models.URLField('URL', unique = TRUE)
+    name = models.CharField(max_length = 200)
+    notes = models.TextField(blank = TRUE)
